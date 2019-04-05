05d2fe9fa2ee62c4b8353ffc8dbfdf28af4b037e
ajb85 <ajbrush85@gmail.com>
2019-04-05 12:42:17 -0400

    Stretch.  CRA'd + added packages

---
+++ dad-jokes/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styles from "./styles.module.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.App}>
+        <p>Hello World</p>
+      </div>
+    );
+  }
+}
+
+export default App;
